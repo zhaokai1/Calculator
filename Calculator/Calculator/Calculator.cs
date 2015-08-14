2234161d862c879d8f9fe8808ca8d947557dd727
zhaokai <zhaokai@gridsum.com>
2015-08-14 17:53:54 +0800

    添加加法和减法

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class Calculator
+	{
+
+		public static int Add(int nubmer1, int nubmer2)
+		{
+			return nubmer1 + nubmer2;
+		}
+		public static int Sub(int number1, int number2)
+		{
+			return number1 - number2;
+		}
+	}
+}
